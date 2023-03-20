d5f7ce77f459641446e356463114e450968fdf69
luigicfh <66189982+luigicfh@users.noreply.github.com>
2023-03-19 18:11:58 -0600

    revelo article v2

--- Code.js
+++ Code.js
@@ -1,14 +1,19 @@
+// Creamos instancia de CommunityConnector.
 const cc = DataStudioApp.createCommunityConnector();
 
+// Establecemos el método de autenticación para nuestro conector
 function getAuthType() {
   const AuthTypes = cc.AuthType;
   return cc.newAuthTypeResponse().setAuthType(AuthTypes.NONE).build();
 }
 
+// Si devuelve true los mensajes de errores se muestran en la interfaz de Looker Studio.
+// Si deveulve false el usuario no obtiene ningún contexto del error.
 function isAdminUser() {
   return true;
 }
 
+// Establece que información debe ser brindada por el usuario para que el conector obtega los datos de una API externa.
 function getConfig() {
   const config = cc.getConfig();
   config
@@ -20,6 +25,7 @@ function getConfig() {
   return config.build();
 }
 
+// Define el esquema de datos a generar en Looker Studio.
 function getFields() {
   const fields = cc.getFields();
   const types = cc.FieldType;
@@ -41,20 +47,27 @@ function getFields() {
   return fields;
 }
 
+// Wrapper para devolver el objeto de esquema generado por getFields().
 function getSchema(request) {
   return { schema: getFields().build() };
 }
 
 function getData(request) {
-  var data;
-  var requestedFieldIds = request.fields.map(function (field) {
+  // Variable a la cual asignaremos los datos obtenidos del API.
+  let data;
+  // Array con los nombres de las columnas del esquema de datos.
+  const requestedFieldIds = request.fields.map(function (field) {
     return field.name;
   });
-  var requestedFields = getFields().forIds(requestedFieldIds);
+  // Objeto de tipo Fields que junto a los datos obtenidos de la API devuelve la estructura de datos final.
+  const requestedFields = getFields().forIds(requestedFieldIds);
   try {
+    // Llamada a la API para obtener datos.
     const apiResponse = fetchDataFromApi(request);
+    // Formateo de los datos obtenidos para que su estructura sea igual a la del esquema.
     data = formatData(requestedFields, apiResponse);
   } catch (e) {
+    // Si isAdminUser() devuelve true esta parte del código es la encargada de mostrar el error en Looker Studio
     cc.newUserError()
       .setDebugText("Error fetching data from API. Exception details: \n" + e)
       .setText(
@@ -73,8 +86,11 @@ function fetchDataFromApi(request) {
     method: "get",
     contentType: "application/json",
   };
+  // Podemos utilizar el servicio de PropertiesService para hacer uso de valores que no queremos exponer en el código/
+  // https://developers.google.com/apps-script/reference/properties/properties-service?hl=es-419
   const endpoint =
     PropertiesService.getScriptProperties().getProperty("endpoint");
+  // Accedemos a parámetros de configuración y de fechas por medio del objeto request y creamos nuestro URL.
   const queryParams =
     "account_sid=" +
     request.configParams.account_sid +
@@ -83,13 +99,14 @@ function fetchDataFromApi(request) {
     "&to_date=" +
     request.dateRange.endDate;
   const apiEndpoint = endpoint + queryParams;
-  var response = UrlFetchApp.fetch(apiEndpoint, params);
+  const response = UrlFetchApp.fetch(apiEndpoint, params);
+  // Devolvemos el JSON obtenido de la API.
   return JSON.parse(response);
 }
 
 function formatData(requestedFields, response) {
   return response.map(function (smsData) {
-    var row = [];
+    const row = [];
     requestedFields.asArray().forEach(function (field) {
       switch (field.getId()) {
         case "account_name":
